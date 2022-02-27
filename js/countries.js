fd8d24782c5ff19b79e79838a81b2992bcaa4690
tanim-k <tanimkhan877@gmail.com>
2022-02-27 18:26:59 +0600

    api practice with onclick single comment

--- js/countries.js
+++ js/countries.js
@@ -1,5 +1,5 @@
 const loadCountries = () => {
-    fetch('https://restcountries.com/v3.1/all')
+    fetch('https://restcountries.com/v2/all')
     .then(res => res.json())
     .then(data => displayCountries(data));
 }
@@ -10,15 +10,24 @@ const displayCountries = countries => {
     // }
     const countriesDiv = document.getElementById('countries');
     countries.forEach(country => {
-        console.log(country)
+        // console.log(country)
         const div = document.createElement('div');
         div.classList.add('country')
-        const h3 = document.createElement('h3');
-        h3.innerText = country.altSpellings[0, 1];
-        div.appendChild(h3);
-        const p = document.createElement('p');
-        p.innerText = country.capital;
-        div.appendChild(p);
+        div.innerHTML =`
+         <h3>${country.name}<h3/>
+         <p>${country.capital}<p/>
+         <button onclick="loadCountryByName('${country.name}')">details</button>
+        `;
+        // const h3 = document.createElement('h3');
+        // h3.innerText = country.altSpellings[0, 1];
+        // div.appendChild(h3);
+        // const p = document.createElement('p');
+        // p.innerText = country.capital;
+        // div.appendChild(p);
         countriesDiv.appendChild(div);
     });
+}
+const loadCountryByName = name => {
+    // fetch('')
+    console.log(name)
 }
