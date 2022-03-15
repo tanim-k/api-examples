5190ad93f9d697718d416db99f2b92490df66132
tanim-k <tanimkhan877@gmail.com>
2022-03-15 23:15:34 +0600

    MEALDB API SITE

--- js/mealBd.js
+++ js/mealBd.js
@@ -3,21 +3,33 @@ const searchField = document.getElementById('button-search');
 const searchInput = document.getElementById('search-input');
 searchInput.addEventListener("keypress", function (event) {
     console.log('keypress trigerred', event.key)
-    if (event.key === 'Enter')
+    if (event.key === 'Enter'){
         searchField.click();
+    }      
 });
 
 const searchFood = () => {
     const searchInput = document.getElementById('search-input');
     const searchInputValue = searchInput.value;
     searchInput.value = '';
+    if(searchInputValue == ''){
+        // make an error message
+    }
+    else{
+        // load data 
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInputValue}`
     fetch(url)
         .then(res => res.json())
         .then(data => displaySearchResult(data.meals));
+    }
 }
 const displaySearchResult = meals => {
     const searchResult = document.getElementById('search-result');
+    searchResult.textContent = '';
+    if(meals.length ==0){
+        // write an error message to ui 
+    }
+    // searchResult.innerHTML = '';
     meals.forEach(meal => {
         // console.log(meal);
         const div = document.createElement('div');
